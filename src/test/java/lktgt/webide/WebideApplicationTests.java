7bf1e424b7a75432f4478c42f2804ec9a7937ed2
lktgt15 <lktgt15@gmail.com>
2020-08-17 07:56:27 +0900

    Initial commit

---
+++ src/test/java/lktgt/webide/WebideApplicationTests.java
@@ -0,0 +1,13 @@
+package lktgt.webide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

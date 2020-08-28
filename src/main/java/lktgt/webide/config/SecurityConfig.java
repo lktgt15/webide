60f36a9380de18b43e2c1f88e7dbf2a58f48c777
lktgt15 <lktgt15@gmail.com>
2020-08-28 12:09:57 +0900

    로그인 권한,로그아웃까지 확인

--- src/main/java/lktgt/webide/config/SecurityConfig.java
+++ src/main/java/lktgt/webide/config/SecurityConfig.java
@@ -29,14 +29,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
             .authorizeRequests()
             .antMatchers("/**").permitAll()
             .anyRequest().permitAll()
         .and()
             .formLogin()
             .loginPage("/login")
-            .usernameParameter("name");
+            .usernameParameter("name")
+        .and()
+            .logout()
+            .logoutSuccessUrl("/");
     }
     
     @Override
